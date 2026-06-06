75e6db0b651edd0e54c6c69274178ac768244a6c
JanFont00 <jan.font@uvic.cat>
2026-06-06 13:10:26 +0200

    Add files via upload

--- Software/Programs/emissor_Definitiu.ino
+++ Software/Programs/emissor_Definitiu.ino
@@ -1,35 +1,56 @@
+/**
+ * PROJECTE: Sistema de politja i ganxo per a dron
+ * MÒDUL: Emissor (Comandament a distància)
+ * DESCRIPCIÓ: Llegeix l'estat de 4 polsadors i envia les ordres de moviment 
+ * i control de la pinça al receptor mitjançant el protocol ESP-NOW.
+ */
+
 #include <esp_now.h>
 #include <WiFi.h>
 
+// --- CONFIGURACIÓ DE XARXA ---
+// Adreça MAC de la placa receptora. S'ha de canviar si es canvia de placa.
 uint8_t broadcastAddress[] = {0x8C, 0xBF, 0xEA, 0x8E, 0xDD, 0x6C}; 
 
-const int pinB1 = 4;
-const int pinB2 = 5;
-const int pinB3 = 6;
-const int pinB4 = 43;
+// --- DEFINICIÓ DE PINS ---
+const int pinB1 = 4;   // Polsador 1: Pujar cable (Enrotllar)
+const int pinB2 = 5;   // Polsador 2: Baixar cable (Desenrotllar)
+const int pinB3 = 6;   // Polsador 3: Obrir pinça
+const int pinB4 = 43;  // Polsador 4: Tancar pinça
 
+// --- ESTRUCTURA DE DADES ---
+// El paquet de dades ha de ser idèntic a l'emissor i al receptor
 typedef struct {
-  bool motorA;
-  bool motorB;
-  bool pinca;
+  bool motorA; // Estat del relé de pujada
+  bool motorB; // Estat del relé de baixada
+  bool pinca;  // Estat del servomotor de la pinça
 } struct_message;
 
 struct_message dades;
 bool estatPinca = false;
-unsigned long ultimDebounce = 0;
+unsigned long ultimDebounce = 0; // Variable per al control d'antirebots (debounce)
 
 void setup() {
   Serial.begin(115200);
+
+  // Configurem els pins dels botons amb resistència pull-up interna
+  // L'estat per defecte serà HIGH, i en prémer el botó serà LOW
   pinMode(pinB1, INPUT_PULLUP);
   pinMode(pinB2, INPUT_PULLUP);
   pinMode(pinB3, INPUT_PULLUP);
   pinMode(pinB4, INPUT_PULLUP);
   
+  // Configurem el Wi-Fi en mode Estació (necessari per a ESP-NOW)
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
-  if (esp_now_init() != ESP_OK) return;
+  // Inicialitzem el protocol ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error inicialitzant ESP-NOW");
+    return;
+  }
   
+  // Registrem el dispositiu receptor (Peer)
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
@@ -38,9 +59,12 @@ void setup() {
 }
 
 void loop() {
+  // Llegeix l'estat dels botons de moviment continus
   bool estatB1 = (digitalRead(pinB1) == LOW);
   bool estatB2 = (digitalRead(pinB2) == LOW);
 
+  // Control de la pinça amb antirebots (250 ms de marge)
+  // Permet que una sola pulsació curta canviï l'estat sense fer lectures dobles
   if (digitalRead(pinB3) == LOW && (millis() - ultimDebounce > 250)) {
     estatPinca = true;
     ultimDebounce = millis();
@@ -51,12 +75,18 @@ void loop() {
     ultimDebounce = millis();
   }
 
+  // Només enviem dades si hi ha hagut algun canvi en l'estat dels botons
+  // Això estalvia bateria i no satura la freqüència de ràdio
   if (dades.motorA != estatB1 || dades.motorB != estatB2 || dades.pinca != estatPinca) {
     dades.motorA = estatB1;
     dades.motorB = estatB2;
     dades.pinca = estatPinca;
+    
+    // Enviem el paquet de dades cap al receptor
     esp_now_send(broadcastAddress, (uint8_t *) &dades, sizeof(dades));
     Serial.printf("Enviat -> MotorA:%d | MotorB:%d | Pinca:%d\n", dades.motorA, dades.motorB, dades.pinca);
   }
+  
+  // Petita pausa per estabilitat del microcontrolador
   delay(10);
 }
--- Software/Programs/receptor_Definitiu.ino
+++ Software/Programs/receptor_Definitiu.ino
@@ -1,15 +1,25 @@
+/**
+ * PROJECTE: Sistema de politja i ganxo per a dron
+ * MÒDUL: Receptor (Integrat al xassís del dron)
+ * DESCRIPCIÓ: Rep ordres de l'emissor via ESP-NOW i controla un pont en H amb
+ * relés per moure el motor principal, a més d'un servomotor per a la pinça.
+ * Inclou lògica d'enclavament de seguretat amb final de carrera magnètic.
+ */
+
 #include <esp_now.h>
 #include <WiFi.h>
 #include <ESP32Servo.h>
 
+// --- CONFIGURACIÓ DE COMPONENTS ---
 Servo pincaServo;
-const int pinServo = 1;
+const int pinServo = 1;  // Pin de senyal per al servomotor (Pinça)
 
-const int releA = 9;
-const int releB = 8;
+const int releA = 9;     // Pin per al Relé A (Pujar/Enrotllar cable)
+const int releB = 8;     // Pin per al Relé B (Baixar/Desenrotllar cable)
 
-const int pinSensor = 2;
+const int pinSensor = 2; // Pin per al Sensor Magnètic (Final de carrera)
 
+// --- ESTRUCTURA DE DADES ---
 typedef struct {
   bool motorA;
   bool motorB;
@@ -18,76 +28,101 @@ typedef struct {
 
 struct_message dadesRebudes;
 
+// --- VARIABLES D'ESTAT ---
 bool ordreMotorA = false;
 bool ordreMotorB = false;
 
+// Variables per gestionar l'enclavament de seguretat (Safety Interlock)
 bool estatBloqueigPujada = false;
 unsigned long tempsIniciBaixada = 0;
 bool baixantActualment = false;
 
+// --- FUNCIÓ DE RECEPCIÓ DE DADES ---
+// Directiva de compilació per mantenir compatibilitat amb versions v2 i v3 del core ESP32
 #if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
 void alRebre(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
 #else
 void alRebre(const uint8_t *mac, const uint8_t *data, int len) {
 #endif
 
+  // Copiem la informació rebuda a la nostra estructura de dades
   memcpy(&dadesRebudes, data, sizeof(dadesRebudes));
   ordreMotorA = dadesRebudes.motorA;
   ordreMotorB = dadesRebudes.motorB;
 
+  // Accionament del servomotor de la pinça
   if (dadesRebudes.pinca == true) {
-    pincaServo.write(70);
+    pincaServo.write(70); // Grau d'obertura de la pinça
   } else {
-    pincaServo.write(0);
+    pincaServo.write(0);  // Grau de tancament de la pinça
   }
 }
 
 void setup() {
   Serial.begin(115200);
 
+  // Configuració de pins de sortida i entrada
   pinMode(releA, OUTPUT);
   pinMode(releB, OUTPUT);
-  pinMode(pinSensor, INPUT_PULLUP);
+  pinMode(pinSensor, INPUT_PULLUP); // El sensor tanca a GND, per tant usem PULLUP
 
+  // Garantim que els motors estiguin aturats a l'inici per seguretat
   digitalWrite(releA, LOW);
   digitalWrite(releB, LOW);
 
+  // Configuració del servomotor (Freqüència i polsos específics per a l'ESP32)
   ESP32PWM::allocateTimer(0);
   pincaServo.setPeriodHertz(50);
   pincaServo.attach(pinServo, 500, 2400);
 
+  // Inicialització de la ràdio en mode Estació
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
 
-  if (esp_now_init() != ESP_OK) return;
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error inicialitzant ESP-NOW");
+    return;
+  }
 
+  // Vinculem la funció que s'executarà automàticament cada cop que arribin dades
   esp_now_register_recv_cb(alRebre);
 }
 
 void loop() {
+  // 1. LECTURA DEL FINAL DE CARRERA
+  // Si l'imant s'apropa al sensor, el pin cau a LOW i s'activa el bloqueig de seguretat
   bool imantDetectat = (digitalRead(pinSensor) == LOW);
 
   if (imantDetectat) {
     estatBloqueigPujada = true;
   }
 
+  // 2. GESTIÓ DEL DESBLOQUEIG (Alliberament de 2 segons)
+  // Si estem demanant baixar, calculem quant de temps mantenim el botó premut.
+  // Això evita que el cable es quedi encallat al límit i obliga a crear un marge físic segur.
   if (ordreMotorB) {
     if (!baixantActualment) {
+      // Iniciem el cronòmetre de baixada
       baixantActualment = true;
       tempsIniciBaixada = millis();
     } else {
+      // Comprovem si ja hem superat els 2 segons de baixada ininterrompuda
       if (millis() - tempsIniciBaixada >= 2000) {
-        estatBloqueigPujada = false;
+        estatBloqueigPujada = false; // Lliurem el bloqueig
       }
     }
   } else {
+    // Si l'usuari deixa anar el botó abans de 2 segons, reiniciem el comptador
     baixantActualment = false;
   }
 
+  // 3. ACTUALITZACIÓ DEL PONT EN H (RELÉS)
   if (estatBloqueigPujada) {
+    // Mode d'emergència: Bloquegem la pujada (Relé A) però permetem la baixada (Relé B)
     digitalWrite(releA, LOW);
     digitalWrite(releB, ordreMotorB ? HIGH : LOW);
   } else {
+    // Mode normal: Apliquem directament les ordres que arriben de l'emissor
     digitalWrite(releA, ordreMotorA ? HIGH : LOW);
     digitalWrite(releB, ordreMotorB ? HIGH : LOW);
   }
